4ac0ec0a7df25123132fc2dc7447ac9b1e0ece61
jcwilk <jcwilk@gmail.com>
2011-07-17 12:08:51 -0700

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompositionEngine::Application.config.secret_token = '285c5a7f97b1fb252b86172cb762ff7a824e05aed630098d49bee4efb041ed0c8b47bad0f01852c7610436cc53cb331daa7502bbe4deccd8ebe7a3a63781df86'
